6bef48d1d466f84b68a1d932da05e1997db3c23c
WangWesley <wangshifu46@outlook.com>
2019-04-15 18:55:40 +0200

    text to speech for android

--- EssentialsDemo/EssentialsDemo/TextToSpeechDemo.cs
+++ EssentialsDemo/EssentialsDemo/TextToSpeechDemo.cs
@@ -21,6 +21,7 @@ namespace EssentialsDemo
         private Picker picker_locale;
         private Locale locale;
         private IEnumerable<Locale> locales;
+        private Button button_getLocale;
 
         public TextToSpeechDemo()
         {
@@ -39,12 +40,70 @@ namespace EssentialsDemo
             instructer2 = new Label { Text = "pitch" };
             result = new Label();
             picker_locale = new Picker();
-            if (Device.RuntimePlatform == "IOS")
+
+
+            if (Device.RuntimePlatform.Equals(Device.Android))
             {
-                //
-            } 
+                button_getLocale = new Button { Text = "Get locales." };
+                button_getLocale.Clicked += Button_getLocale_Clicked;
+
+
+                StackLayout stloPic = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    Children = { picker_locale }
+                };
+
+                Content = new StackLayout
+                {
+                    Children = { title, instructer1, slider_volume, instructer2, slider_pitch, button_getLocale, stloPic, entry, button_speak, result }
+                };
+            }
+            else // other platforms
+            {
+                // equal to: IEnumerable<Locale> locales = await TextToSpeech.GetLocalesAsync(); 
+                GetLocaleAsync();
+
+                string s = "";
+                foreach (Locale value in locales)
+                {
+                    Console.WriteLine("locale: " + value.ToString());
+                    s += value.Language.ToString() + "; ";
+                }
+
+                Console.WriteLine("Triggered.");
+
+                // Grab the first locale
+                locale = locales.FirstOrDefault();
+                picker_locale.SelectedIndex = 0;
+                s += "FirstOrDefault: " + locales.FirstOrDefault().Language.ToString();
+                result.Text = s;
+
+                foreach (Locale localeValue in locales)
+                {
+                    picker_locale.Items.Add(localeValue.Language);
+                }
+                picker_locale.SelectedIndexChanged += Picker_locale_SelectedIndexChanged;
+
+                StackLayout stloPic = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    Children = { picker_locale }
+                };
+
+                Content = new StackLayout
+                {
+                    Children = { title, instructer1, slider_volume, instructer2, slider_pitch, stloPic, entry, button_speak, result }
+                };
+            }
+
+        }
+
+        private void Button_getLocale_Clicked(object sender, EventArgs e)
+        {
             // equal to: IEnumerable<Locale> locales = await TextToSpeech.GetLocalesAsync(); 
             GetLocaleAsync();
+
             string s = "";
             foreach (Locale value in locales)
             {
@@ -65,19 +124,6 @@ namespace EssentialsDemo
                 picker_locale.Items.Add(localeValue.Language);
             }
             picker_locale.SelectedIndexChanged += Picker_locale_SelectedIndexChanged;
-
-            StackLayout stloPic = new StackLayout
-            {
-                Orientation = StackOrientation.Horizontal,
-                Children = { picker_locale }
-            };
-
-            Content = new StackLayout
-            {
-                Children = {
-                    title, instructer1, slider_volume, instructer2, slider_pitch, stloPic, entry, button_speak, result
-                }
-            };
         }
 
         private void Picker_locale_SelectedIndexChanged(object sender, EventArgs e)
